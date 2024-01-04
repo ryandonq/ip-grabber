4e84fd464d9e21a25ef882c18bd1eddb7f529c77
01 <110252997+ryandonq@users.noreply.github.com>
2024-01-03 22:48:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-rr
+import requests
+import json
+from datetime import date
+
+client_id = ""
+client_secret = ""
+redirect = ""
+
+def get_ip_address():
+    ip = requests.get('https://api.ipify.org').text
+    return ip
+  
